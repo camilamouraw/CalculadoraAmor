143366dfc8375e6897445aa692faad177f82a33a
unknown <camilaaoliver10@gmail.com>
2022-04-01 22:35:12 -0300

    prj sem CSS

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  nome1=""
+  nome2=""
+  url="http://lucasreno.kinghost.net/love-calculator/"
+  resultado = 0
+  mensagem = ""
+
+
+  constructor(
+    public http: HttpClient,
+  ) {}
+
+  CalcularAmor() {
+    this.http.get<any>(this.url+this.nome1 + "/" + this.nome2).subscribe(
+      (resposta:any) =>{
+        this.resultado = resposta;
+        if(this.resultado < 20) this.mensagem = "Muito iludido tadinho(a)!";
+        else if( this.resultado >20 && this.resultado < 40 ) this.mensagem = "Vai que uma hora dá certo";
+        else if( this.resultado >40 && this.resultado < 60 ) this.mensagem = "Existe chances, mas nada sério";
+        else if( this.resultado >60 && this.resultado < 80 ) this.mensagem = "Aproveita ele(a) ta na sua !";
+        else this.mensagem = " Oww um casal perfeito !!!"
+
+      }
+    );
+  }
+  
+  
+}
