02cfdc768b6c20aa2e88f254c2d691f237634166
camilamouraw <camilaaoliver10@gmail.com>
2022-04-08 22:15:53 -0300

    Prj finalizado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,18 +13,32 @@ export class HomePage {
   url="http://lucasreno.kinghost.net/love-calculator/"
   resultado = 0
   mensagem = ""
-
+  //imagem = false
+  calculando = false;
 
   constructor(
     public http: HttpClient,
   ) {}
 
-  CalcularAmor() {
+
+    async enviarDados() {
+      let soma = 0;
+      //this.imagem = false
+      while (soma != 10) {
+        this.resultado = Math.floor(Math.random() * 100 + 1);
+        this.calculando = true;
+        soma += 1;
+        await this.delay(75);
+      }
+      this.calculando = false;
+
+      
+  
     this.http.get<any>(this.url+this.nome1 + "/" + this.nome2).subscribe(
       (resposta:any) =>{
         this.resultado = resposta;
         if(this.resultado < 20) this.mensagem = "Muito iludido tadinho(a)!";
-        else if( this.resultado >20 && this.resultado < 40 ) this.mensagem = "Vai que uma hora dá certo";
+        else if( this.resultado >20 && this.resultado < 40 ) this.mensagem = "0 chances você tem !";
         else if( this.resultado >40 && this.resultado < 60 ) this.mensagem = "Existe chances, mas nada sério";
         else if( this.resultado >60 && this.resultado < 80 ) this.mensagem = "Aproveita ele(a) ta na sua !";
         else this.mensagem = " Oww um casal perfeito !!!"
@@ -32,6 +46,10 @@ export class HomePage {
       }
     );
   }
+
+        delay(ms: number) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+}
   
   
 }
